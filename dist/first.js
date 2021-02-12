67d55854a9dc996e889aa3ac6d7fee6f87a558a1
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-12 22:15:35 +0600

    added set function in first.ts file

--- dist/first.js
+++ dist/first.js
@@ -1,4 +1,17 @@
 "use strict";
-let myMoney = 232;
-myMoney = 324;
-myMoney = 324;
+let age;
+let club = 'real madrid';
+const isFamous = false;
+age = 23;
+console.log(age);
+console.log(club);
+// function add(x: number | string, y: number | string): string {
+//   return x + y;
+// }
+// add(12, 12);
+// add('joney', 'talukdar');
+function fullName(firstName, lastName) {
+    return firstName + ' ' + lastName;
+}
+const user = fullName('Martin ', 'keleabba');
+console.log(user);
--- first.ts
+++ first.ts
@@ -1,3 +1,22 @@
-let myMoney = 232;
-myMoney = 324;
-myMoney = 324;
+let age: number;
+let club: string = 'real madrid';
+const isFamous: boolean = false;
+
+age = 23;
+
+console.log(age);
+console.log(club);
+
+// function add(x: number | string, y: number | string): string {
+//   return x + y;
+// }
+
+// add(12, 12);
+// add('joney', 'talukdar');
+
+function fullName(firstName: string, lastName: string) {
+  return firstName + ' ' + lastName;
+}
+
+const user: string = fullName('Martin ', 'keleabba');
+console.log(user);
