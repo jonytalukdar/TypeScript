481043750de6e323f8efce22a48b434d70666531
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-12 22:52:19 +0600

    added some line about array and object

--- dist/first.js
+++ dist/first.js
@@ -15,14 +15,41 @@ function fullName(firstName, lastName) {
 }
 const user = fullName('Martin ', 'keleabba');
 console.log(user);
+// type of empty function
 function doubleIt(num) {
     const result = num * 2;
     console.log(result);
 }
 const output = doubleIt(12);
 console.log(output);
+// typeof array function
 let multiplyTwo;
 multiplyTwo = (x, y) => x * y;
 console.log(multiplyTwo(12, 12));
 const multiply = (x, y) => x * y;
 console.log(multiply(12, 12));
+// set type of array and object and see the structure
+const numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14];
+numbers.push(15);
+const friends = ['joney', 'jerry', 'jubayer', 'roney'];
+let megaFriend = '';
+for (let i = 0; i < friends.length; i++) {
+    const friend = friends[i];
+    if (friend.length > megaFriend.length) {
+        megaFriend = friend;
+    }
+}
+console.log('friends with largest name :', megaFriend);
+// object
+let player;
+player = {
+    club: 'Real Madrid',
+    salary: 321412432,
+};
+console.log(player);
+const objFriend = {
+    name: 'joney talukdar',
+    age: 21,
+};
+objFriend.age = 62;
+objFriend.name = 'jerry talukdar';
--- first.ts
+++ first.ts
@@ -21,6 +21,8 @@ function fullName(firstName: string, lastName: string) {
 const user: string = fullName('Martin ', 'keleabba');
 console.log(user);
 
+// type of empty function
+
 function doubleIt(num: number): void {
   const result = num * 2;
   console.log(result);
@@ -28,9 +30,47 @@ function doubleIt(num: number): void {
 const output = doubleIt(12);
 console.log(output);
 
+// typeof array function
+
 let multiplyTwo: (x: number, y: number) => number;
 multiplyTwo = (x, y) => x * y;
 console.log(multiplyTwo(12, 12));
 
 const multiply = (x: number, y: number): number => x * y;
 console.log(multiply(12, 12));
+
+// set type of array and object and see the structure
+
+const numbers: number[] = [2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14];
+numbers.push(15);
+
+const friends: string[] = ['joney', 'jerry', 'jubayer', 'roney'];
+let megaFriend: string = '';
+for (let i = 0; i < friends.length; i++) {
+  const friend: string = friends[i];
+  if (friend.length > megaFriend.length) {
+    megaFriend = friend;
+  }
+}
+console.log('friends with largest name :', megaFriend);
+
+// object
+
+let player: {
+  club: string;
+  salary: number;
+};
+
+player = {
+  club: 'Real Madrid',
+  salary: 321412432,
+};
+console.log(player);
+
+const objFriend: { name: string; age: number } = {
+  name: 'joney talukdar',
+  age: 21,
+};
+
+objFriend.age = 62;
+objFriend.name = 'jerry talukdar';
